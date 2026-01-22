83dfdfbf28c9484d9b1dd155da6c713eb9c1639d
Dawson Toth <dawson@harperdb.io>
2026-01-22 17:09:24 -0500

    fix: Simplify file copying methods so we can pass substitutions to copy dir too

--- templates-shared/applySharedTemplates.js
+++ templates-shared/applySharedTemplates.js
@@ -4,11 +4,6 @@ import path from 'node:path';
 import colors from 'picocolors';
 import { copyDir } from '../lib/fs/copyDir.js';
 
-const {
-	cyan,
-	green,
-} = colors;
-
 (async function() {
 	const copiesToMake = {
 		'all': [
@@ -35,7 +30,6 @@ const {
 		for (const targetTemplate of copiesToMake[key]) {
 			const toTemplate = path.resolve(import.meta.dirname, '..', targetTemplate);
 			copyDir(fromShared, toTemplate);
-			console.log(`Copied ${cyan(fromShared)} to ${green(toTemplate)}`);
 		}
 	}
 })();
