182b8ad19c7f006f1e67478bc6eed56513211a51
junie-agent <247260674+junie-agent@users.noreply.github.com>
2026-03-02 19:40:32 +0000

    feat: Create Vue and Vue-TS Project Templates
    Co-authored-by: dawsontoth <472869+dawsontoth@users.noreply.github.com>

---
+++ template-vue/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import typescriptLogo from '/typescript.svg';
+import viteLogo from '/vite.svg';
+import vueLogo from '/vue.svg';
+import { ref } from 'vue';
+
+const counter = ref(0);
+const countUp = () => {
+	counter.value++;
+};
+</script>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
+      <img :src="viteLogo" class="logo" alt="Vite logo" />
+    </a>
+    <a
+      href="https://www.typescriptlang.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img :src="typescriptLogo" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">
+      <img :src="vueLogo" class="logo vue" alt="Vue logo" />
+    </a>
+    <h1>Vite + TypeScript + Vue</h1>
+    <p>Wow, look at this!</p>
+    <div class="card">
+      <button id="counter" type="button" @click="countUp">
+        count is {{ counter }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
