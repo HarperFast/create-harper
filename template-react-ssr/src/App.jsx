6db29e799fc2e14d340db847a369062c5273a68d
Dawson Toth <dawson@harperdb.io>
2026-06-17 14:41:35 -0400

    fix: Remove irrelevant typescript references

--- template-react-ssr/src/App.jsx
+++ template-react-ssr/src/App.jsx
@@ -1,5 +1,4 @@
 import reactLogo from '/react.svg';
-import typescriptLogo from '/typescript.svg';
 import viteLogo from '/vite.svg';
 import { useCallback, useState } from 'react';
 import { increment } from './counter.js';
@@ -16,13 +15,10 @@ export function App() {
 				<a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
 				</a>
-				<a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
-					<img src={typescriptLogo} className="logo vanilla" alt="TypeScript logo" />
-				</a>
 				<a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
-				<h1>Vite + TypeScript + React</h1>
+				<h1>Vite + React</h1>
 				<p>Wow, look at this!</p>
 				<div className="card">
 					<button id="counter" type="button" onClick={countUp}>
--- template-vue-ssr/src/App.vue
+++ template-vue-ssr/src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import typescriptLogo from '/typescript.svg';
 import viteLogo from '/vite.svg';
 import vueLogo from '/vue.svg';
 import { ref } from 'vue';
@@ -16,17 +15,10 @@ const countUp = () => {
     <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
       <img :src="viteLogo" class="logo" alt="Vite logo" />
     </a>
-    <a
-      href="https://www.typescriptlang.org/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img :src="typescriptLogo" class="logo vanilla" alt="TypeScript logo" />
-    </a>
     <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">
       <img :src="vueLogo" class="logo vue" alt="Vue logo" />
     </a>
-    <h1>Vite + TypeScript + Vue</h1>
+    <h1>Vite + Vue</h1>
     <p>Wow, look at this!</p>
     <div class="card">
       <button id="counter" type="button" @click="countUp">
